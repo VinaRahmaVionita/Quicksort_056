65beced343fad3d0cfb8a518c6e57702c48a38c0
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-03-31 15:56:27 +0700

    menambhakan prosedur void display dengan menampilkan jumlah perbandingan dan jumlah data pindah

--- Quicksort_056/Quicksort_056.cpp
+++ Quicksort_056/Quicksort_056.cpp
@@ -92,3 +92,18 @@ void q_short(int low, int high)
 	//sort the list on the right of the pivot using quickl sort
 	q_short(j + 1, high); //langkah 13 
 }
+
+void display() {
+	cout << "\n-------------------------------" << endl;
+	cout << "Sorted array" << endl;
+	cout << "---------------------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+
+}
