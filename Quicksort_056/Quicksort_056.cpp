2aebb73c58c56917b2f60ce68a26abee820b56bb
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-03-31 15:46:43 +0700

    memindahkan pivot ke posisi yang benar di dalam list

--- Quicksort_056/Quicksort_056.cpp
+++ Quicksort_056/Quicksort_056.cpp
@@ -79,4 +79,11 @@ void q_short(int low, int high)
 			mov_count++;
 		}
 	}
+	// j now containt the index of the last element in the sorted list 
+	if (low < j) //langkah 11
+	{
+		//move the pivot to its correct position in the list 
+		swap(low, j);
+		mov_count++;
+	}
 }
